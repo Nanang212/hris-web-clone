2d7e82e695f96a8e4e57edcbbce668af19693939
Tubagus Saifulloh <dutapanggulu@gmail.com>
2026-09-06 05:56:23 +0700

    feat(signout): adjust flow

--- src/routes/(app)/route.tsx
+++ src/routes/(app)/route.tsx
@@ -1,10 +1,15 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 import { AppNavbar } from '@/shared/components/app-layout/app-navbar'
 import { AppSidebar } from '@/shared/components/app-layout/app-sidebar'
 import { SidebarProvider } from '@/shared/components/ui/sidebar'
+import { getCookie } from '@/shared/lib/utils'
 
 export const Route = createFileRoute('/(app)')({
+  beforeLoad: async () => {
+    const isSignedIn = getCookie('is_signed_in') === 'true'
+    if (!isSignedIn) throw redirect({ to: '/signin', replace: true })
+  },
   component: RouteComponent,
 })
 
@@ -12,7 +17,7 @@ function RouteComponent() {
   return (
     <SidebarProvider className='bg-sidebar'>
       <AppSidebar />
-      <div className='flex-1 min-w-0 w-full overflow-x-hidden'>
+      <div className='w-full min-w-0 flex-1 overflow-x-hidden'>
         <AppNavbar className='w-full' user={{ name: 'John Doe', role: 'Admin' }} />
         <Outlet />
       </div>
